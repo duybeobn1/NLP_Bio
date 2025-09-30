45f6f0b831333aa92744e57cbb0b93ae1297a0d3
Vũ Anh Duy <duybeobn1@MacBook-Pro.local>
2025-09-30 11:31:00 +0200

    RNN

--- RNN_Model.py
+++ RNN_Model.py
@@ -7,15 +7,15 @@ from p1 import *
 class CustomRNN(nn.Module):
     def __init__(self, input_size, emb_size, hidden_size, output_size=6):
         super(CustomRNN, self).__init__()
-        self.i2e = nn.Linear(input_size, emb_size)
-        self.hidden_size = hidden_size
+        self.i2e = nn.Linear(input_size, emb_size) # input as input_size with the same size of vocab (one-hot) -> reduce to emb_size( numbers of dimensions )
+        self.hidden_size = hidden_size 
         self.rnn = nn.RNN(emb_size, hidden_size, batch_first=True)
         self.h2o = nn.Linear(hidden_size, output_size)
         self.softmax = nn.LogSoftmax(dim=1)
 
     def forward(self, line_tensor):
         # line_tensor shape: (batch_size, seq_len, input_size)
-        embedded = self.i2e(line_tensor)            # (batch, seq_len, emb_size)
+        embedded = self.i2e(line_tensor)            # (batch, seq_len, emb_size) 
         rnn_out, hidden = self.rnn(embedded)        # rnn_out: (batch, seq_len, hidden_size)
         output = self.h2o(hidden.squeeze(0))        # hidden.shape: (num_layers=1, batch, hidden_size)
                                                     # take the last hidden state
